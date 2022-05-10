e5b57df04c97c2583a1f39cbb242501c50389d75
PonomareVlad <Git@PonomareVlad.ru>
2022-05-11 03:19:18 +0500

    Persistent storage for Bot Users

--- bot.mjs
+++ bot.mjs
@@ -1,11 +1,23 @@
 import TeleBot from "telebot"
+import mongo from './db.mjs'
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
-bot.on('text', msg => msg.text.startsWith('/') ? null : msg.reply.text(msg.text))
-
-bot.on('/start', (msg) => msg.reply.photo('https://picsum.photos/1000'));
-
 bot.on('/env', (msg) => msg.reply.text(process.env.VERCEL_ENV));
+bot.on('text', async msg => {
+    if (msg.text.startsWith('/')) return;
+    let user = await fetchUser(msg.chat)
+    const data = {id: user.id, count: user.count ? ++user.count : 1}
+    user = await fetchUser(data)
+    return msg.reply.text(`${msg.text} — ${user.count}`)
+})
 
 export default bot
+
+const users = mongo.db('Telegram').collection('Users')
+
+async function fetchUser(data) {
+    const filter = {id: data.id}
+    await users.updateOne(filter, {$set: data}, {upsert: true})
+    return await users.findOne(filter)
+}
