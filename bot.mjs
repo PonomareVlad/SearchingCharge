dd0887614afca0853b8197d83815abed3bec0da7
PonomareVlad <Git@PonomareVlad.ru>
2022-05-11 02:14:36 +0500

    Env command

--- bot.mjs
+++ bot.mjs
@@ -2,8 +2,10 @@ import TeleBot from "telebot"
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
-bot.on('text', msg => msg.text.startsWith('/')?null:msg.reply.text(msg.text))
+bot.on('text', msg => msg.text.startsWith('/') ? null : msg.reply.text(msg.text))
 
 bot.on('/start', (msg) => msg.reply.photo('https://picsum.photos/1000'));
+
+bot.on('/env', (msg) => msg.reply.text(process.env.VERCEL_ENV));
 
 export default bot
