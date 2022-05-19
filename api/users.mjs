cabdbca8455ca5839ad7fb21eb11723b2c626cd3
PonomareVlad <Git@PonomareVlad.ru>
2022-05-19 15:21:35 +0500

    Fixed download headers

--- api/users.mjs
+++ api/users.mjs
@@ -2,10 +2,12 @@ import mongo from '../src/db.mjs'
 
 const users = mongo.db('Telegram').collection('Users')
 
-export default async (req, {send}) => {
+export default async (req, res) => {
+    res.setHeader('Content-Type', 'text/csv')
+    res.setHeader('Content-Disposition', 'attachment; filename=Users.csv')
     const usersList = await users.find().toArray()
     const header = ['ID', 'Логин', 'Имя', 'Фамилия', 'Финалист']
-    return send([header, ...usersList.map(renderUserData)].join('\r\n'))
+    return res.send([header, ...usersList.map(renderUserData)].join('\r\n'))
 }
 
 const renderUserData = ({id, username, first_name, last_name, final} = {}) =>
